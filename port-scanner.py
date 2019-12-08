7eba1962fcbb791409f3b07f26f200d27a80357d
MK-Kaiser <51086253+MK-Kaiser@users.noreply.github.com>
2019-12-08 16:13:48 -0600

    Added logic to handle http(s) ports differently

--- port-scanner.py
+++ port-scanner.py
@@ -7,7 +7,11 @@ def connScan(tgtHost, tgtPort):
     try:
         connSock = socket(AF_INET, SOCK_STREAM)
         connSock.connect((tgtHost, tgtPort))
-        connSock.send(b'HEAD / HTTP/1.1\r\r\n\n')
+        if tgtPort == 80 or 443:
+            connSock.send(b'HEAD / HTTP/1.1\r\r\n\n')
+        else:
+            connSock.send(b'connect request\r\n')
+
         results = connSock.recv(100)
         print('[+]%d/tcp open'% tgtPort)
         print('[+] ' + str(results))
@@ -38,17 +42,22 @@ def portScan(tgtHost, tgtPorts):
 #argument parser to construct help menu and handle user supplied values
 def main():
     parser = argparse.ArgumentParser(description='scan a host for running services')
-    parser.add_argument('-u', '--url', dest='tgtHost', type=str, required=True, help='provide a target url, or address example: www.website.com')
+    parser.add_argument('-u', '--url', dest='tgtHost', type=str, required=False, help='provide a target url, or address example: www.website.com')
     parser.add_argument('-v', '--version', dest='ver', required=False, action='store_true', help='display version number.')
-    parser.add_argument('-p', '--ports', dest='tgtPort', type=str, required=True, help='provide a port or comma separated list')
+    parser.add_argument('-p', '--ports', dest='tgtPort', required=False, type=str, help="provide port[s] example: '22, 80' or 22 ")
     args = parser.parse_args()
     tgtHost = args.tgtHost
     tgtPorts = str(args.tgtPort).split(', ')
+    if args.ver:
+        print("portScanner version 0.1")
+        exit()
 
     if (tgtHost == None) | (tgtPorts[0] == None):
         print('[-] You must specify a target host and port[s]')
         exit(0)
     portScan(tgtHost, tgtPorts)
-    
+
+
+
 if __name__ == '__main__':
     main()
